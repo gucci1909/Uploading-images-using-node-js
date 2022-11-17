bb2b0942b00ed63884a562bc13dc1ad38682f97b
gucci1909 <umangarora0134@gmail.com>
2022-11-17 21:16:31 +0530

    backend for uploading images

--- index.js
+++ index.js
@@ -2,17 +2,41 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const cors = require("cors");
-const mongoose = require("mongoose");
-const songRoutes = require("./Routes/Songs/song.route")
+const multer = require("multer");
 
 app.use(cors());
 app.use(express.json());
+app.use(
+  express.urlencoded({ extended: true })
+);
 
-app.use("/songs",songRoutes);
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "Images");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
 
-app.get("/",(req,res)=>{
-    res.send("Hello World!");
-})
+  });
+  const upload = multer({ storage: storage });
+
+  //post image
+  app.post("/images", upload.single("image"), (req, res) => {
+    const file = req.file;
+    if (!file) {
+      const error = new Error("Please upload a file");
+      error.httpStatusCode = 400;
+      return next(error);
+    }
+    res.send(file);
+  });
+
+  //get images
+  app.get("/images", (req, res) => {
+    res.send("Images");
+  });
+
 
 app.listen(PORT , ()=>{
     console.log(`http://localhost:${PORT}`);
